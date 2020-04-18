d1a4ff5d85f9a11d177432e72c1c47518975d9ad
JustinAMiddleton <stngry09@msn.com>
2020-04-18 10:55:59 -0400

    Google Scholar failure...

--- connectmybib.py
+++ connectmybib.py
@@ -1,10 +1,7 @@
+import json
 import scholarly
 import sys
 import xml.etree.ElementTree as ET
-
-# read in existing analyzed DB
-
-# read in current bib
 
 
 def readBib(file):
@@ -17,6 +14,7 @@ def readBib(file):
                         record.findall("./contributors/authors/author")]
         })
     return papers
+
 
 def findPaper(author, title):
     title = title.lower()
@@ -38,21 +36,22 @@ def findPaper(author, title):
                 if b.bib["title"].lower() == title:
                     return b.fill()
         except StopIteration:
-            print("\t\t%s has an empty bib!")
+            print("\t\t%s has an empty bib!")  # I need a test case.
             continue
 
     return None
 
 
-bib = readBib("C:/Users/jalan/git/GiantShoulders/My Collection.xml")
-bib = [{"authors": ["Andy Podgurski"],
-        "title": "Retrieving Reusable Software by Sampling Behavior"}]
-for paper in bib:
+def findCitedBy(paper):
     title = paper["title"]
     print(title)
 
+    # I reverse because: younger researchers go up front and might be less often
+    #   to have a Scholar profile. Elder researchers go in the back, often.
     # sorted(paper["authors"], key=len, reverse=True):
     for author in paper["authors"][::-1]:
+        # Names arrive in that Robertson, Bob form. Will middle initials
+        #   interfere?
         author = " ".join(author.split(", ")[::-1])
 
         scholar = findPaper(author, title)
@@ -65,8 +64,27 @@ for paper in bib:
                 print("\tWho knows how cited???")
 
             citedby = scholar.get_citedby()
-            print([x for x in citedby])
-            break
+            return citedby
+
+
+if __name__ == "__main__":
+    bib = readBib("C:/Users/jalan/git/GiantShoulders/My Collection.xml")
+    bib = [{"authors": ["Andy Podgurski"],
+            "title": "Retrieving Reusable Software by Sampling Behavior"}]
+    for paper in bib:
+        citedby = findCitedBy(paper)
+        paper["citedby"] = [{"authors": paper["author"],
+                             "title": paper["title"],
+                             "year": paper["year"],
+                             "journal": paper["journal"],
+                             "citedby": paper.citedby}]
+
+SAMPLEBIB = [{"authors": ["Andy Podgurski"],
+              "title": "Retrieving Reusable Software by Sampling Behavior"}]
+
+# Current failure: I do ONE of these queries and Scholar doesn't like it! 😡
+#   I don't think there's a way around it; they don't want scraping like I'm
+#       trying to do, even though it's through a library.
 
 # Current failure: empty get_citedby sets.
 #   In get_citedby(): hasattr(self, 'id_scholarcitedby') returns false after fill.
